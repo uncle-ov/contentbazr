0bfe94d2063f22cc3925dfac6894903e42ac2246
Overcomer <overcomeridemudia@gmail.com>
2023-10-12 23:08:32 +0100

    debug

--- resources/views/includes/users.blade.php
+++ resources/views/includes/users.blade.php
@@ -41,7 +41,7 @@ if (auth()->check()) {
 				<div class="row">
 
 					<div class="col-4 px-1">
-								{{dd($images[0]->thumbnail)}}
+								{{dd(json_decode($images[0]->thumbnail))}}
 								<img src="{{ isset($images[0]) ? Storage::url(config('path.thumbnail').$images[0]->thumbnail->name) : asset('public/img/placeholder.jpg') }}" width="95" class="image-card-user me-1">
 					</div>
 
